18d3ac3964d8ffc4ed02cc87992f5a2006ff941f
Matthias Franz <matthiasfranz197@gmx.de>
2026-05-28 02:19:49 +0000

    refactor: paths

--- python/utils/import_noisy_queries.py
+++ python/utils/import_noisy_queries.py
@@ -1,29 +1,53 @@
 import numpy as np
 import pandas as pd
 from glob import glob
+import os
 
 
-def import_noisy_queries(collection, n_queries):
-    '''
-    mechanisms = ["MahalanobisMechanism", "CMPMechanism", "VickreyMMechanism", "VickreyCMPMechanism"]
+def import_noisy_queries(collection, n_queries, mechanism=None):
+    
+    #mechanisms = ["MahalanobisMechanism", "CMPMechanism", "VickreyMMechanism", "VickreyCMPMechanism"]
+    mechanisms = [mechanism] if mechanism else ["MahalanobisMechanism", "CMPMechanism", "VickreyMMechanism", "VickreyCMPMechanism"]
     for m in mechanisms:
-        df = import_pd_noisy_queries(collection, m, n_queries)
-        df['qid'] = df.apply(lambda x: f"{x['qid']}_{x['rep']}", axis=1)
-        for e in df['eps'].unique():
-            df.loc[df.eps == e][['qid', 'query']].to_csv(f"../../data/queries/{collection}/valid/{m}_{e}.csv", header=False, index=False, sep=";")
+        output_dir = f"../../data/queries/{collection}/valid"
+        os.makedirs(output_dir, exist_ok=True)
 
+        print(f"Processing folder for mechanism: {m}")
+        df = import_pd_noisy_queries(collection, m, n_queries)
+
+        if df is None or df.empty:
+            print(f"No data found for mechanism: {m} in collection: {collection}")
+            continue
+
+        df['qid'] = df.apply(lambda x: f"{x['qid']}_{x['rep']}", axis=1)
+        #print(df)
+        df['eps'] = df['eps'].astype(float)
+        #print(df[df['qid'] == '301_7'].sort_values(by='eps', ascending=False))
+
+        for e in df['eps'].unique():
+            output_file = f"{output_dir}/{m}_{e}.csv"
+            df.loc[df.eps == e][['qid', 'query']].to_csv(output_file, header=False, index=False, sep=";")
+            print(f"Saved combined queries for mechanism: {m} with epsilon: {e} to {output_file}")
+    '''
     scramblers = ["ArampatzisEtAl2011", "FroebeEtAl2021"]
+    '''
     '''
     scramblers = ["FroebeEtAl2021b"]
     for s in scramblers:
         df = import_sota_noisy_queries(collection, s, n_queries)
         df['qid'] = df.apply(lambda x: f"{x['qid']}_{x['rep']}", axis=1)
         df[['qid', 'query']].to_csv(f"../../data/queries/{collection}/valid/{s}.csv", header=False, index=False, sep=";")
-
+    '''
 
 def import_pd_noisy_queries(collection, mechanism, n_queries):
-    files = glob(f"../../data/queries/{collection}/{mechanism}/*.csv")
+    search_pattern = f"../../data/queries/{collection}/{mechanism}/*.csv"
+    files = glob(search_pattern)
     files = [f for f in files if int(f.rsplit("_", 1)[1].split(".")[0]) < n_queries]
+
+    if not files:
+        print(f"No files found for mechanism: {mechanism} in collection: {collection}")
+        return None
+
 
     dfs = []
     for f in files:
@@ -71,6 +95,8 @@ def import_sota_noisy_queries(collection, scrambler, n_queries, **kwargs):
 
 import argparse
 parser = argparse.ArgumentParser()
-parser.add_argument("-c", "--collection")
+parser.add_argument("-c", "--collection", default="robust04")
+parser.add_argument("-m", "--mechanism", default="MahalanobisMechanism")
+parser.add_argument("-n", "--num_perturbations", default=20, type=int)
 args = parser.parse_args()
-import_noisy_queries(args.collection, 20)
+import_noisy_queries(args.collection, args.num_perturbations, args.mechanism)
