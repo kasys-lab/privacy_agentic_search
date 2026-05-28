197d5e0fcc697f5db05908325745748e8df8ca06
Matthias Franz <matthiasfranz197@gmx.de>
2026-05-28 09:34:18 +0000

    Bug fix: script was not compatible with new python version and multiprocessing

--- python/experiments/number_of_relevants.py
+++ python/experiments/number_of_relevants.py
@@ -16,11 +16,11 @@ collections = {'robust04': 'irds:disks45/nocr/trec-robust-2004',
                'trec-covid': 'irds:beir/trec-covid',
                'msmarco-passage': 'irds:msmarco-passage/trec-dl-2019/judged'}
 
-if not pt.started():
-    pt.init()
+if not pt.java.started():
+    pt.java.init()
 
 dataset = pt.get_dataset(collections[args.collection])
-paths = glob(f"../../data/runs/{args.collection}_*")
+paths = glob(f"../../data/runs/{args.collection}*")
 paths = [p for p in paths if "glove" not in p]
 
 qrels = dataset.get_qrels().drop(["iteration"], axis=1).rename({"qid": "query_id", "docno": "doc_id", "label": "relevance"}, axis=1)
@@ -51,48 +51,50 @@ def parse_run(path):
     run['epsilon'] = float(epsilon)
     return run
 
+if __name__ == "__main__":
+    with Pool(processes=32) as pool:
+        future = [pool.apply_async(parse_run, [p]) for p in paths]
+        runs = [fr.get() for fr in future]
 
-with Pool(processes=70) as pool:
-    future = [pool.apply_async(parse_run, [p]) for p in paths]
-    runs = [fr.get() for fr in future]
-
-runs = pd.concat(runs)
-relevant_retrieved = runs[['query_id', 'doc_id', 'model', 'mechanism', 'epsilon']].drop_duplicates().groupby(
-    ['query_id', 'model', 'mechanism', 'epsilon'], dropna=False).count().reset_index().rename({"doc_id": 'retrieved'}, axis=1)
-recall_base = relevant[['query_id', 'doc_id']].groupby(["query_id"]).count().reset_index().rename({"doc_id": "recall_base"}, axis=1)
-relevant_retrieved = relevant_retrieved.merge(recall_base)
-relevant_retrieved['perc'] = relevant_retrieved['retrieved'] / relevant_retrieved['recall_base']
-print(relevant_retrieved[relevant_retrieved.mechanism.str.contains("Mechanism")][['mechanism', 'epsilon', 'model', 'perc']]
-      .groupby(['mechanism', 'epsilon', 'model'], dropna=False)
-      .mean().reset_index()
-      .pivot_table(index=["model", "mechanism"], columns="epsilon", values="perc")
-      .to_string())
-print(relevant_retrieved[~relevant_retrieved.mechanism.str.contains("Mechanism")][['mechanism', 'model', 'perc']]
-      .groupby(['model', 'mechanism'], dropna=False)
-      .mean().reset_index()
-      .pivot_table(index="mechanism", columns="model", values="perc")
-      .to_string())
-# for each model and for each query, take the documents retrieved at least once
-'''
-# for each mechanism, how many unique relevants are retrieved?
-rel_retrieved_times = runs[["query_id", "model", "doc_id", "mechanism", "epsilon", "rep"]].groupby(
-    ["query_id", "model", "doc_id", "mechanism", "epsilon"], dropna=False).count().reset_index()
-rel_retrieved_times = relevant.merge(rel_retrieved_times, how="left", on=["query_id", "doc_id"])
-
-
-def plot_hist(df):
-    out = df[['query_id', 'doc_id', 'rep']].fillna(0)
-    print(out)
+    runs = pd.concat(runs)
+    relevant_retrieved = runs[['query_id', 'doc_id', 'model', 'mechanism', 'epsilon']].drop_duplicates().groupby(
+        ['query_id', 'model', 'mechanism', 'epsilon'], dropna=False).count().reset_index().rename({"doc_id": 'retrieved'}, axis=1)
+    recall_base = relevant[['query_id', 'doc_id']].groupby(["query_id"]).count().reset_index().rename({"doc_id": "recall_base"}, axis=1)
+    relevant_retrieved = relevant_retrieved.merge(recall_base)
+    relevant_retrieved['perc'] = relevant_retrieved['retrieved'] / relevant_retrieved['recall_base']
+    print(relevant_retrieved[relevant_retrieved.mechanism.str.contains("Mechanism")][['mechanism', 'epsilon', 'model', 'perc']]
+        .groupby(['mechanism', 'epsilon', 'model'], dropna=False)
+        .mean().reset_index()
+        .pivot_table(index=["model", "mechanism"], columns="epsilon", values="perc")
+        .round(3)
+        .to_string())
+    print(relevant_retrieved[~relevant_retrieved.mechanism.str.contains("Mechanism")][['mechanism', 'model', 'perc']]
+        .groupby(['model', 'mechanism'], dropna=False)
+        .mean().reset_index()
+        .pivot_table(index="mechanism", columns="model", values="perc")
+        .round(3)
+        .to_string())
+    # for each model and for each query, take the documents retrieved at least once
+    '''
+    # for each mechanism, how many unique relevants are retrieved?
+    rel_retrieved_times = runs[["query_id", "model", "doc_id", "mechanism", "epsilon", "rep"]].groupby(
+        ["query_id", "model", "doc_id", "mechanism", "epsilon"], dropna=False).count().reset_index()
+    rel_retrieved_times = relevant.merge(rel_retrieved_times, how="left", on=["query_id", "doc_id"])
 
 
-for model in ['BM25', 'TF-IDF', 'glove', 'tasb', 'contriever']:
-    for mechanism in ['CMPMechanism', 'MahalanobisMechanism', 'VikreyCMPMechanism', 'VikreyMMechanism', 'ArampatzisEtAl2011']:
-        if "Mechanism" in mechanism:
-            for epsilon in [1., 5., 10., 12.5, 15., 17.5, 20., 35., 50.]:
-                plot_hist(rel_retrieved_times[((rel_retrieved_times.model == model) & (rel_retrieved_times.mechanism == mechanism) & (
-                        rel_retrieved_times.epsilon == epsilon))])
-        else:
-            plot_hist(rel_retrieved_times[((rel_retrieved_times.model == model) & (rel_retrieved_times.mechanism == mechanism))])
+    def plot_hist(df):
+        out = df[['query_id', 'doc_id', 'rep']].fillna(0)
+        print(out)
+
+
+    for model in ['BM25', 'TF-IDF', 'glove', 'tasb', 'contriever']:
+        for mechanism in ['CMPMechanism', 'MahalanobisMechanism', 'VikreyCMPMechanism', 'VikreyMMechanism', 'ArampatzisEtAl2011']:
+            if "Mechanism" in mechanism:
+                for epsilon in [1., 5., 10., 12.5, 15., 17.5, 20., 35., 50.]:
+                    plot_hist(rel_retrieved_times[((rel_retrieved_times.model == model) & (rel_retrieved_times.mechanism == mechanism) & (
+                            rel_retrieved_times.epsilon == epsilon))])
+            else:
+                plot_hist(rel_retrieved_times[((rel_retrieved_times.model == model) & (rel_retrieved_times.mechanism == mechanism))])
+            break
         break
-    break
-'''
+    '''
